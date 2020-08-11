7ed4e7e31063e0bdc966875227c697108f852a11
Majed Itani <mitani@setsail.co>
2020-08-11 15:30:52 -0700

    remove google dlp

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,4 @@
 import { SyncCompositeRedactor, SyncCompositeRedactorOptions } from './SyncCompositeRedactor';
 import { AsyncCompositeRedactor, AsyncCompositeRedactorOptions } from './AsyncCompositeRedactor';
-import { GoogleDLPRedactor, GoogleDLPRedactorOptions } from './custom/GoogleDLPRedactor';
 import { AsyncCustomRedactorConfig, CompositeRedactorOptions, IAsyncRedactor, ISyncRedactor, IRedactor, SimpleRegexpCustomRedactorConfig } from './types';
-export { SyncCompositeRedactor as SyncRedactor, SyncCompositeRedactorOptions, AsyncCompositeRedactor as AsyncRedactor, AsyncCompositeRedactorOptions, GoogleDLPRedactor, GoogleDLPRedactorOptions, AsyncCustomRedactorConfig, CompositeRedactorOptions, IAsyncRedactor, ISyncRedactor, IRedactor, SimpleRegexpCustomRedactorConfig };
+export { SyncCompositeRedactor as SyncRedactor, SyncCompositeRedactorOptions, AsyncCompositeRedactor as AsyncRedactor, AsyncCompositeRedactorOptions, AsyncCustomRedactorConfig, CompositeRedactorOptions, IAsyncRedactor, ISyncRedactor, IRedactor, SimpleRegexpCustomRedactorConfig };
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,1 @@
-declare module '@google-cloud/dlp';
+
